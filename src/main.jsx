6629847a31afe0309d5034292c7da8579b353adb
camilla novofjeld <cafj001@student.kristiania.no>
2026-09-01 15:38:18 +0200

    create new task

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById("root")).render(<h1>Hello React</h1>);
+function Application() {
+  const [tasks, setTask] = useState([
+    { name: "Create React app" },
+    { name: "Create new task" },
+    { name: "Mark task as done" },
+  ]);
+  const [name, setName] = useState("");
+  return (
+    <>
+      <h1>My task application</h1>
+      <ul>
+        {tasks.map((t) => (
+          <li>{t.name}</li>
+        ))}
+      </ul>
+      <h2>new tasck</h2>
+      <form action="">
+        <div>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div>
+          <button>save {name}</button>
+        </div>
+      </form>
+    </>
+  );
+}
+
+createRoot(document.getElementById("root")).render(<Application />);
